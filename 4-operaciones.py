5e75e9db42156323967cccb7bd6d682dc10d2107
corderoangel <corderoan777@gmail.com>
2024-10-18 08:16:13 -0500

    operaciones

--- 4-operaciones.py
+++ 4-operaciones.py
@@ -1,28 +1,52 @@
-#Operadores numéricos
+# Declaración de variables
 a = 10
 b = 10
-print("Suma:", a + b)
-print("Resta:", a - b)
-print("Multiplicación:", a * b)
-print("Potenciación:", a ** b)
-print("División:", a / b)
-print("Parte entera de la división:", a // b)
-print("Módulo:", a % b)
-a /= 2
-print(a)
-operation_1  = 2 + 3 * 4
-operation_2  = (2 + 3) * 4
-print(operation_1)
-print(operation_2)
-operation_3 = (2+3) * (4**2)/ 8 - 1
-print(operation_3)
 
-#comparación
+# Suma
+print("Suma:", a + b)  # Resultado: 20
+
+# Resta
+print("Resta:", a - b)  # Resultado: 0
+
+# Multiplicación
+print("Multiplicación:", a * b)  # Resultado: 100
+
+# Potenciación
+print("Potenciación:", a ** b)  # Resultado: 10000000000 (10 elevado a la 10)
+
+# División
+print("División:", a / b)  # Resultado: 1.0 (resultado flotante)
+
+# División entera
+print("Parte entera de la división:", a // b)  # Resultado: 1 (resultado entero)
+
+# Módulo (residuo)
+print("Módulo:", a % b)  # Resultado: 0 (residuo de la división de 10 entre 10)
+
+a /= 2
+print(a)  # Resultado: 5.0 (a ahora es un número flotante)
+
+# Operación sin paréntesis, sigue la precedencia de operadores (multiplicación antes que suma)
+operation_1  = 2 + 3 * 4
+print(operation_1)  # Resultado: 14 (primero se multiplica 3 * 4 y luego se suma 2)
+
+# Operación con paréntesis, altera la precedencia
+operation_2  = (2 + 3) * 4
+print(operation_2)  # Resultado: 20 (primero se suma 2 + 3 y luego se multiplica por 4)
+
+# Operación compleja con diferentes operadores y precedencias
+operation_3 = (2+3) * (4**2) / 8 - 1
+print(operation_3)  # Resultado: 11.0
+
+
+# Redefinición de las variables
 a = 10
 b = 3
-print(a > b)
-print(a < b)
-print(a >= b)
-print(a <= b)
-print(a == b)
-print(a != b)
+
+# Comparaciones
+print(a > b)   # Resultado: True (10 es mayor que 3)
+print(a < b)   # Resultado: False (10 no es menor que 3)
+print(a >= b)  # Resultado: True (10 es mayor o igual que 3)
+print(a <= b)  # Resultado: False (10 no es menor o igual que 3)
+print(a == b)  # Resultado: False (10 no es igual a 3)
+print(a != b)  # Resultado: True (10 es diferente de 3)
