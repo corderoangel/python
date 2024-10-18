9165cc9d63c970e33706dac74292ab5b145f2a32
corderoangel <corderoan777@gmail.com>
2024-10-18 10:06:56 -0500

    lambda

--- 15-lambda.py
+++ 15-lambda.py
@@ -1,14 +1,25 @@
+# Definición de funciones lambda para operaciones matemáticas
+
+# Sumar dos números
 add = lambda a, b: a + b
-print(add(10,4))
+# Imprimir el resultado de la suma de 10 y 4
+print(add(10, 4))
 
+# Multiplicar dos números
 multiply = lambda a, b: a * b
-print(multiply(80,5))
+# Imprimir el resultado de la multiplicación de 80 y 5
+print(multiply(80, 5))
 
-#Cuadrado de cada numero
-numbers = range(11)
+# Cuadrado de cada número en un rango
+numbers = range(11)  # Rango de números del 0 al 10
+
+# Usar map para aplicar la función lambda que calcula el cuadrado a cada número en el rango
 squared_numbers = list(map(lambda x: x**2, numbers))
-print("Cuadrados:", squared_numbers )
+# Imprimir la lista de cuadrados
+print("Cuadrados:", squared_numbers)
 
-#Pares
-even_numbers = list(filter(lambda x: x%2 == 0, numbers))
+# Filtrar los números pares
+# Usar filter para aplicar la función lambda que verifica si un número es par
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+# Imprimir la lista de números pares
 print("Pares:", even_numbers)
