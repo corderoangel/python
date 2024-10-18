d9d1a923f4592cad49efbce9caa0261cf32bc243
corderoangel <corderoan777@gmail.com>
2024-10-18 08:30:21 -0500

    listas pt 2

--- 8-listas2.py
+++ 8-listas2.py
@@ -1,11 +1,13 @@
 # más dimensiones y tuplas
-matrix = [[1,2,3],
-          [4,5,6],
-          [7,8,9]]
-print(matrix[2][1])
-numbers = 1,2,3,4,5
-print(numbers)
-print(type(numbers))
-print(numbers[0])
-#numbers[0] = 'unos'
+matrix = [[1, 2, 3],  # Fila 1
+          [4, 5, 6],  # Fila 2
+          [7, 8, 9]]  # Fila 3
+print(matrix[2][1])  # Imprime: 8 (Elemento en la fila 3, columna 2)
+
+numbers = 1, 2, 3, 4, 5  # Definición de una tupla
+print(numbers)  # Imprime: (1, 2, 3, 4, 5)
+print(type(numbers))  # Imprime: <class 'tuple'>
+print(numbers[0])  # Imprime: 1 (Primer elemento de la tupla)
+
+#numbers[0] = 'unos'  # Intento de modificación de una tupla
 #print(numbers)
