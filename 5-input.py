0c362f588e04f49695c816e704eb195d600f2e32
corderoangel <corderoan777@gmail.com>
2024-10-18 08:17:58 -0500

    input

--- 5-input.py
+++ 5-input.py
@@ -1,5 +1,14 @@
+# Solicita al usuario ingresar su nombre
 name = input("Ingrese su nombre: ")
+
+# Muestra el nombre ingresado por el usuario
 print(name)
-print(type(name))
+
+# Muestra el tipo de dato de la variable 'name'
+print(type(name))  # Resultado: <class 'str'>
+
+# Solicita al usuario ingresar su edad y convierte el valor a entero
 age = int(input("Ingrese su edad: "))
-print(type(age))
+
+# Muestra el tipo de dato de la variable 'age'
+print(type(age))  # Resultado: <class 'int'>
