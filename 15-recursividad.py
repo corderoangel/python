afcefba4715bf0144a096806c1d0973a89d92346
corderoangel <corderoan777@gmail.com>
2024-10-18 10:08:35 -0500

    recursividad

--- 15-recursividad.py
+++ 15-recursividad.py
@@ -1,11 +1,30 @@
 def sum_numbers(n):
+    """
+    Calcula la suma de los primeros n números enteros de forma recursiva.
+
+    Args:
+        n (int): Un número entero no negativo.
+
+    Returns:
+        int: La suma de los primeros n números enteros.
+    
+    Raises:
+        ValueError: Si n es un número negativo.
+    """
+    
+    # Verifica si el número es negativo y lanza una excepción
+    if n < 0:
+        raise ValueError("El número debe ser no negativo.")
+    
     # Caso base: si n es 0, la suma es 0
     if n == 0:
         return 0
+    
     # Caso recursivo: n + suma de (n-1)
     else:
         return n + sum_numbers(n - 1)
 
-# Llamada a la función
+# Llamada a la función para calcular la suma de los primeros 5 números
 result = sum_numbers(5)
+# Imprimir el resultado
 print(f"Suma de los primeros 5 números es: {result}")
