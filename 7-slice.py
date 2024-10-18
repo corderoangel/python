2ea00c6c6e314ef7e31634433d61377f239a742f
corderoangel <corderoan777@gmail.com>
2024-10-18 08:28:44 -0500

    slice

--- 7-slice.py
+++ 7-slice.py
@@ -1,15 +1,18 @@
-a = [1,2,3,4,5]
-b = a
-print(a)
-print(b)
-del a[0]
-print(id(a))
-print(id(b))
-c = a[:]
-print(id(a))
-print(id(b))
-print(id(c))
-a.append(6)
-print(a)
-print(b)
-print(c)
+a = [1, 2, 3, 4, 5]  # Lista original
+b = a  # Asignación directa: b apunta al mismo objeto que a
+print(a)  # Imprime: [1, 2, 3, 4, 5]
+print(b)  # Imprime: [1, 2, 3, 4, 5]
+
+del a[0]  # Elimina el primer elemento de la lista 'a'
+print(id(a))  # Imprime el identificador único del objeto 'a' en memoria
+print(id(b))  # Imprime el mismo identificador que 'a', ya que ambos apuntan al mismo objeto
+
+c = a[:]  # Crea una copia superficial de 'a'
+print(id(a))  # Identificador de 'a'
+print(id(b))  # Identificador de 'b', igual al de 'a'
+print(id(c))  # Identificador de 'c', diferente, ya que es una nueva lista
+
+a.append(6)  # Agrega el número 6 al final de la lista 'a'
+print(a)  # Imprime: [2, 3, 4, 5, 6]
+print(b)  # Imprime: [2, 3, 4, 5, 6], ya que 'b' aún es una referencia de 'a'
+print(c)  # Imprime: [2, 3, 4, 5], ya que 'c' es una copia independiente
